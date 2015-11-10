a027493a8f97ed6ca6e5eb3cb4b7657e2245b0da
lukadubravcic <lukadubravcic@yahoo.com>
2015-11-10 17:11:57 +0100

    restclient7

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ $app->get('/api/posts', function () {
     echo "<h1>Welcome!</h1>";
 	
 	$api = new RestClient(array(
-    'base_url' => "vinkovic.riteh.hexis.hr", 
+    'base_url' => "tic984.riteh.hexis.hr", 
     'format' => "json"
 	));
 	
-	$result = $api->get("/api/posts", array('name' => "test123"));
+	$result = $api->get("/api/posts"));
 	
 	echo $result->decode_response();
 	
