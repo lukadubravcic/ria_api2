817ec5fc9a894d05fc2b0cc702cf51362e6e7d3f
lukadubravcic <lukadubravcic@yahoo.com>
2015-11-10 16:47:19 +0100

    restclient

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+include "restclient.php"
 use Phalcon\Mvc\Micro;
 
 $app = new Micro();
 
 $app->get('/say/{name}', function ($name) {
     echo "<h1>Welcome $name!</h1>";
+	
+	$api = new RestClient(array(
+    'base_url' => "vinkovic.riteh.hexis.hr", 
+    'format' => "json"
+	));
+	
+	$result = $api->get("/api/posts", array('naslov' => "mirovanje"));
+	echo $result;
+	
 });
 
 $app->handle();
