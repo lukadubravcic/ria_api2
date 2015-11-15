39a04445147622089e62f25633f8f4f69e99710d
lukadubravcic <LukaDubravcic@yahoo.com>
2015-11-16 00:46:56 +0100

    Update display.php

--- display.php
+++ display.php
@@ -5,9 +5,10 @@ class display_entries
 	public function displ($json_array)
 	{
 		echo "<table>";
+		echo "<tr><td>ID</td> <td>NASLOV</td> <td>DATE</td></tr>";
 		foreach ($json_array as $item){
 			echo'<tr><td>',$item->id,'</td><td>',$item->naslov,'</td><td>',$item->datum,'</td></tr>';
-}
+		}
 		echo "</table";
 	}
 
