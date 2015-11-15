b878d7950f5a9028d64fd4514714e30a13be89db
lukadubravcic <LukaDubravcic@yahoo.com>
2015-11-16 00:54:17 +0100

    Update display.php

--- display.php
+++ display.php
@@ -7,7 +7,7 @@ class display_entries
 		echo "<table>";
 		echo "<tr><td>ID</td> <td>NASLOV</td> <td>DATE</td></tr>";
 		foreach ($json_array as $item){
-			echo'<tr><td>',$item->id,'</td><td>',$item->naslov,'</td><td>',$item->datum,'</td></tr>';
+			echo "<tr><td>",$item->id,"</td><td>",$item->naslov,"</td><td>",$item->datum,"</td></tr>";
 		}
 		echo "</table";
 	}
