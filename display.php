34ea881a1bd4810d39e61ffb03e8755b70c55302
lukadubravcic <LukaDubravcic@yahoo.com>
2015-11-16 00:29:53 +0100

    Syntax

--- display.php
+++ display.php
@@ -5,8 +5,8 @@ class display_entries
 	public function displ($json_array)
 	{
 		echo "<table>";
-		foreach ($json_array as $k=>$v){
-			echo'<tr><th>',$k,'</th><td>',$v,'</td></tr>';
+		foreach ($json_array as $item){
+			echo'<tr><td>',$item->id,'</td><td>',$item->naslov,'</td><td>',$item->datum,'</td></tr>';
 }
 		echo "</table";
 	}
