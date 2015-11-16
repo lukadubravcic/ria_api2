93ad8e583ac3cc98ce858ef73e50c6af72a3d6c9
lukadubravcic <LukaDubravcic@yahoo.com>
2015-11-16 01:58:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $app->get('/api/posts', function () {
 	//echo "<br />";
 	echo "<table>";
 	echo "<tr><td>ID</td> <td>NASLOV</td> <td>DATE</td></tr>";
-	foreach ($json_array as $item){
+	foreach ($decoded as $item){
 		echo "<tr><td>",$item->id,"</td><td>",$item->naslov,"</td><td>",$item->datum,"</td></tr>";
 	}
 	echo "</table";
