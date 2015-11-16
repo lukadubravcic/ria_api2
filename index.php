1f603cf1f41a53f21c197f863a056063e2fa13b8
lukadubravcic <LukaDubravcic@yahoo.com>
2015-11-16 12:22:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,33 +19,17 @@ $app->get('/api/posts', function () {
 	$decoded = $result->decode_response();
 	
 	displ($decoded);
-
-	/*echo "<table>";
-	echo "<tr><td>ID</td> <td>NASLOV</td> <td>DATE</td></tr>";
-	foreach($decoded as $item) {
-		echo "<tr><td>$item->id</td><td>$item->naslov</td><td>$item->datum</td></tr>";
-	}
-	echo "</table";*/
 });
-/*
+
 $app->get('/api/post/{param}', function ($param) {
-    //echo "<h1>Welcome!</h1>";
-	
+    
 	$api = new RestClient(array(
-    'base_url' => "vinkovic.riteh.hexis.hr", 
-    //'format' => "json"
-	));
+    		'base_url' => "vinkovic.riteh.hexis.hr", 
+    	));
 	
 	$result = $api->post("/api/post", array('naslov' => $param));
 	
-	$obj = new display_entries;
-	$obj->displ(json_encode($result->decode_response()));
-	
-	
-	
-	
 });
-*/
 
 
 $app->handle();
